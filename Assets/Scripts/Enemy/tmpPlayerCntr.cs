7a1fbb5ccded9853b82f9add24ad432810b22926
hujyyyy <jhu377@gatech.edu>
2020-02-01 09:45:50 -0500

    basic enemy AI

---
+++ Assets/Scripts/Enemy/tmpPlayerCntr.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tmpPlayerCntr : MonoBehaviour
+{
+    Rigidbody m_rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        m_rb.AddForce(10*new Vector3(horizontalInput, 0, verticalInput));
+    }
+}
