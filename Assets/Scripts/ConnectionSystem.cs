06282132c5e8c92b0aeb258e44027544970ad02c
Jacob Light <jakestar0306@comcast.net>
2020-01-31 22:50:28 -0500

    Connection system

---
+++ Assets/Scripts/ConnectionSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionSystem : MonoBehaviour {
+
+	// add this to both the player and any attachable objects
+
+	public enum State {
+		loose,
+		connected
+	};
+	public State currState = State.loose;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+    void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        ConnectionSystem cs = other.gameObject.GetComponent<ConnectionSystem>();
+        if (currState == ConnectionSystem.State.loose && cs != null && cs.currState == ConnectionSystem.State.connected) {
+             // rb.constraints = RigidbodyConstraints.FreezeRotation;
+        	// transform.SetParent(other.gameObject.transform, false);
+        	transform.SetParent(GM.gm.player.transform, false);
+        	currState = ConnectionSystem.State.connected;
+        	rb.velocity = Vector3.zero;
+        	rb.angularVelocity = Vector3.zero;
+        	rb.constraints = RigidbodyConstraints.FreezePosition;
+        	rb.constraints = RigidbodyConstraints.FreezeRotation;
+        }
+    }
+}
---
+++ Assets/Scripts/ConnectionSystemTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionSystemTest : MonoBehaviour {
+	private Rigidbody rb;
+
+	// Use this for initialization
+    void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+    	rb.AddForce(new Vector3(40,0,0));
+    }
+}
