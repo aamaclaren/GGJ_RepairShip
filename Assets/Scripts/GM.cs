66292ce5e4e47c6a594e266cf6b366e6b3058fbb
Jacob Light <jakestar0306@comcast.net>
2020-02-01 14:01:58 -0500

    More connection system + added health to GM

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -6,6 +6,13 @@ public class GM : MonoBehaviour
 {
     public static GM gm;
     public GameObject player;
+    public int playerHealth = 100;
+    public Material hurtMaterial;
+    public ConnectionSystem playerCS;
+
+    private bool isInvincible = false;
+    private Material defaultPlayerMaterial;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +24,26 @@ public class GM : MonoBehaviour
         {
             gm = this;
         }
+
+        playerCS = player.gameObject.GetComponent<ConnectionSystem>();
+        defaultPlayerMaterial = player.gameObject.GetComponent<Renderer>().material;
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator PlayerTookDamage(int damage = 100)
     {
-        
+        isInvincible = true;
+        playerCS.isConnectable = false;
+        for (int i = 0; i < 5; i++)
+        {
+            player.gameObject.GetComponent<Renderer>().material = hurtMaterial;
+            yield return new WaitForSeconds(.12f);
+            player.gameObject.GetComponent<Renderer>().material = defaultPlayerMaterial;
+            if (i < 4) {
+                yield return new WaitForSeconds(.12f);
+            }
+        }
+        isInvincible = false;
+        playerCS.isConnectable = true;
+
     }
 }
---
+++ Assets/Scripts/Test Scripts/ConnectionSystemTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionSystemTest : MonoBehaviour {
+	public int speedX = 30;
+    public int speedZ = 0;
+    public bool takeDamageFlag = false;
+    public bool stopFlag = false;
+
+	private Rigidbody rb;
+    private ConnectionSystem cs;
+
+	// Use this for initialization
+    void Awake() {
+        rb = GetComponent<Rigidbody>();
+        cs = gameObject.GetComponent<ConnectionSystem>();
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+    	rb.AddForce(new Vector3(speedX,0,speedZ));
+    }
+
+    void Update() {
+        if (takeDamageFlag) {
+            cs.TakeDamage();
+            takeDamageFlag = false;
+        }
+        if (stopFlag) {
+            rb.velocity = Vector3.zero;
+            stopFlag = false;
+        }
+    }
+}
