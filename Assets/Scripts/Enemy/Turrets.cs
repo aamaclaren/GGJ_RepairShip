ba218853396687f2151c4ef207da4a97b4e26c38
Andrew <aho3555@gmail.com>
2020-02-01 15:36:01 -0500

    Refined turret code

--- Assets/Scripts/Enemy/Turrets.cs
+++ Assets/Scripts/Enemy/Turrets.cs
@@ -9,6 +9,8 @@ public class Turrets : MonoBehaviour
     public float firingInterval = .5f;
     LineRenderer line;
     float time;
+    RaycastHit hit;
+    public float damage = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,7 +32,17 @@ public class Turrets : MonoBehaviour
                 line.enabled = true;
                 Ray ray = new Ray(transform.position, transform.forward);
                 line.SetPosition(0, ray.origin);
-                line.SetPosition(1, ray.GetPoint(100));
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Debug.Log(hit.collider.gameObject);
+                    line.SetPosition(1, hit.collider.gameObject.transform.position);
+                    //Damage health here
+                    
+                }
+                else
+                {
+                    line.SetPosition(1, ray.GetPoint(100));
+                }
                 time = 0;
             }
             else
