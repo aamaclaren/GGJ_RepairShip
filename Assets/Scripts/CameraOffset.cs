0a03d0cee5a4f89d9b789c62a66ba657b1a90679
Jacob Light <jakestar0306@comcast.net>
2020-02-02 15:20:04 -0500

    Fixed CameraOffset

--- Assets/Scripts/CameraOffset.cs
+++ Assets/Scripts/CameraOffset.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class CameraOffset : MonoBehaviour
 {
-    Vector3 offset;
-    bool off = false;
+    public float zoomSpeed = 2f;
+    public float intensity = 0.35f;
+
+    private float offset;
+    // Vector3 offset;
+    // bool off = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position;
+        offset = transform.position.y;
     }
 
     // Update is called once per frame
@@ -20,6 +25,17 @@ public class CameraOffset : MonoBehaviour
         {
             transform.position = offset + GM.gm.player.transform.position;
         }*/
-        transform.position = offset + GM.gm.player.transform.position;
+        // transform.position = offset + GM.gm.player.transform.position;
+        MoveCamera(GM.gm.GetLargestRadius() * intensity + offset + GM.gm.player.transform.position);
+    }
+
+    private void MoveCamera(float newPos = 10) {
+        if (Mathf.Abs(transform.position.y - newPos) > 0.01) {
+            int mult = 1;
+            if (transform.position.y > newPos) {
+                mult = -1;
+            }
+            transform.position = new Vector3(transform.position.x, transform.position.y + zoomSpeed * mult * Time.deltaTime, transform.position.z);
+        }
     }
 }
