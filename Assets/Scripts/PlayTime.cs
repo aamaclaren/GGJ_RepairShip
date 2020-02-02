d2823a8c0adb75a0697051aa9c909b9e1fe15632
jmoore180 <jmoore180@student.gsu.edu>
2020-02-02 05:56:07 -0500

    UI Setup

---
+++ Assets/Scripts/PlayTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class PlayAni : MonoBehaviour
+{
+    PlayableDirector play;
+    void Start()
+    {
+        play = GetComponent<PlayableDirector>();
+    }
+
+    void Update()
+    {
+        
+    }
+    
+    public void onClick()
+    {
+        play.Play();
+    }
+}
