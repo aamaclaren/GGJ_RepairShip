bd941d6ed2689cc3dd4b1b11bcf3bcae0e04ce04
Andrew <aho3555@gmail.com>
2020-02-01 20:06:37 -0500

    stuff

---
+++ Assets/Scripts/Test Scripts/RotateChildTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateChildTests : MonoBehaviour
+{
+    ConnectionSystem cs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cs = GetComponent<ConnectionSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cs.currState == ConnectionSystem.State.connected)
+        {
+            transform.LookAt(GM.gm.player.transform.position);
+            transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+        }
+    }
+}
